8bb23b3681616961a5cdaccc3305ef5f7fe72fe8
Erik Shahverdian <13771746+Demigod808@users.noreply.github.com>
2023-07-14 12:33:27 -0700

    Update views.py
    Attempt at importing MySQL Database and routing it to carlist to update car details.

--- Website/views.py
+++ Website/views.py
@@ -1,4 +1,8 @@
 from flask import Blueprint, render_template
+import mysql.connector
+
+import mysql.connector
+from mysql.connector import Error
 
 views = Blueprint('views', __name__)  # create a blueprint named 'views'
 
@@ -18,9 +22,37 @@ def signup():
     return render_template('signup.html')  # render the 'logout.html' template
 
 
-@views.route('/carlist')  # decorator for the logout route
+@views.route('/carlist')
 def carlist():
-    return render_template('carlist.html')  # render the 'logout.html' template
+    try:
+        mydb = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='Demigod808!',
+            port='3306',
+            database='apolloautomotive',
+        )
+
+        mycursor = mydb.cursor()
+
+        mycursor.execute('SELECT * FROM Vehicle')
+        vehicles = mycursor.fetchall()
+
+        for vehicle in vehicles:
+            print(vehicle)
+
+        mycursor.execute('SELECT * FROM Customer')
+        customers = mycursor.fetchall()
+
+        for customer in customers:
+            print(customer)
+
+        return render_template('carlist.html', vehicles=vehicles, customers=customers)
+
+    except Error as e:
+        print("Error connecting to the database:", e)
+
+    return render_template('carlist.html')
 
 @views.route('/login')  # decorator for the logout route
 def login():
